1b395f4e0809d7472380fd146ce00d7d7f7ed465
jeff5m <jeffmatheuspereira@gmail.com>
2021-08-27 20:32:13 -0300

    add: floating action button

--- lib/main.dart
+++ lib/main.dart
@@ -18,17 +18,25 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.deepPurple,
-        appBar: AppBar(
-          backgroundColor: Colors.purple,
-          title: Text("Counter"),
-          centerTitle: true,
+      backgroundColor: Colors.purpleAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: Text("Counter"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          "Meu primeiro texto",
+          style: TextStyle(color: Colors.white, fontSize: 18),
         ),
-        body: Center(
-          child: Text(
-            "Meu primeiro texto",
-            style: TextStyle(color: Colors.white, fontSize: 18),
-          ),
-        ));
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.purple,
+        onPressed: () {
+          print("Clicked!");
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
